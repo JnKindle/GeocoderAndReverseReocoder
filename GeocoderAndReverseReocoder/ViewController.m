26e401f2392afd1937333881d53227478f4b67d7
JnKindle <Jn_never2013@163.com>
2016-12-21 11:55:25 +0800

    地理编码与反地理编码

--- GeocoderAndReverseReocoder/ViewController.m
+++ GeocoderAndReverseReocoder/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import <CoreLocation/CoreLocation.h>
+
 @interface ViewController ()
 
 @end
@@ -16,13 +18,88 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //地理编码
+    [self geocoder];
+    
+    
+    //地理反编码
+    [self reverseGeocoder];
+    
+    
+}
+
+
+/**
+ 地理编码
+ */
+- (void)geocoder {
+    
+    CLGeocoder *geocoder=[[CLGeocoder alloc]init];
+    
+    NSString *addressStr = @"广东省深圳市宝安区";//位置信息
+    
+    [geocoder geocodeAddressString:addressStr completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        if (error!=nil || placemarks.count==0) {
+            return ;
+        }
+        //创建placemark对象
+        CLPlacemark *placemark=[placemarks firstObject];
+        //经度
+        NSString *longitude =[NSString stringWithFormat:@"%f",placemark.location.coordinate.longitude];
+        //纬度
+        NSString *latitude =[NSString stringWithFormat:@"%f",placemark.location.coordinate.latitude];
+        
+        
+        NSLog(@"经度：%@，纬度：%@",longitude,latitude);
+        
+    }];
+    
+}
+
+
+
+/**
+ 地理反编码
+ */
+- (void)reverseGeocoder{
+    //创建地理编码对象
+    CLGeocoder *geocoder=[[CLGeocoder alloc]init];
+    
+    //经度
+    NSString *longitude = @"113.23";
+    //纬度
+    NSString *latitude = @"23.16";
+    
+    
+    //创建位置
+    CLLocation *location=[[CLLocation alloc]initWithLatitude:[latitude floatValue] longitude:[longitude floatValue]];
+    
+    
+    //反地理编码
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        //判断是否有错误或者placemarks是否为空
+        if (error !=nil || placemarks.count==0) {
+            NSLog(@"%@",error);
+            return ;
+        }
+        for (CLPlacemark *placemark in placemarks) {
+            //详细地址
+            NSString *addressStr = placemark.name;
+            NSLog(@"详细地址1：%@",addressStr);
+            NSLog(@"详细地址2：%@",placemark.addressDictionary);
+            NSLog(@"详细地址3：%@",placemark.locality);
+        }
+        
+    }];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+
+
 }
 
 
